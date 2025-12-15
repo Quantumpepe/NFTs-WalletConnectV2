ea067d490c73b8c9c910f485b12c1bbe91cab0af
Fabrizio <fabrizio3030@gmailcom>
2025-12-15 19:19:09 +0100

    Updates

---
+++ assets/if-defined-CcUt9AMJ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-PDYU2NvT.js";const m=o=>o??r;export{m as o};
---
+++ assets/transactions-CJjLWESC.js
@@ -0,0 +1,16 @@
+import{n as f,m as a,x as m}from"./index-PDYU2NvT.js";import{c as p}from"./index-DGFir5tG.js";import"./index-DlJ7ij7f.js";import"./index-CxoSvyTY.js";import"./if-defined-CcUt9AMJ.js";import"./index-B3LDH66M.js";import"./index-CTb21DFG.js";import"./index-1oE9JvY5.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
